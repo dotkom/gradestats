6d0ce0d4be02983bc4361791bb7110f2eb18ec0a
Andrefkl <andre.klarpaas@gmail.com>
2024-12-30 22:35:33 +0100

    refactor: move ntnu scraper to course_pages client

--- clients/client.py
+++ clients/client.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
-
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 class Client:
     session = None
@@ -11,3 +12,19 @@ class Client:
 
     def init_soup(self, page_text: str):
         return BeautifulSoup(page_text, "html5lib")
+
+    def requests_retry_session(
+        retries=3, backoff_factor=0.3, status_forcelist=(500, 502, 504), session=None
+    ):
+        session = session or requests.Session()
+        retry = Retry(
+            total=retries,
+            read=retries,
+            connect=retries,
+            backoff_factor=backoff_factor,
+            status_forcelist=status_forcelist,
+        )
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount("http://", adapter)
+        session.mount("https://", adapter)
+        return session
