74368781041dbbd8424e43be440be47563c45a4e
Andre Klarpås <andre.klarpaas@gmail.com>
2025-02-26 14:17:48 +0100

    fix docker ports

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,15 +3,16 @@ python3 manage.py migrate
 
 echo Starting uwsgi.
 exec uwsgi --chdir=/srv/app \
-    --plugins=python3,http \
     --module=gradestats.wsgi:application \
     --env DJANGO_SETTINGS_MODULE=gradestats.settings.prod \
     --master --pidfile=/tmp/gradestats.pid \
-    --http=0.0.0.0:3000 \
+    --socket=0.0.0.0:8080 \
+    --http=0.0.0.0:8081 \
     --processes=5 \
     --harakiri=300 \
     --max-requests=5000 \
     --offload-threads=4 \
+    --enable-threads \
     --static-map=/static=/srv/app/collected_static \
     --static-map=/media=/srv/app/media \
     --vacuum
