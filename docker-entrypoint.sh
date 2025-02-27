08db70f529507e8cb36bb1f8901f5ce28e782d57
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2025-02-27 20:01:35 +0100

    Fix entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env sh
 uv run python manage.py migrate
 
-DJANGO_SETTINGS_MODULE=gradestats.settings.prod exec uv run daphne \ 
-        --port=8080 \
-        --bind 0.0.0.0 \
-        gradestats.asgi:application
+DJANGO_SETTINGS_MODULE=gradestats.settings.prod exec uv run -- daphne --port=8080 --bind 0.0.0.0 gradestats.asgi:application
